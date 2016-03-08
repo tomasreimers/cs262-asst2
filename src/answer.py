d2d09d26b11eb2f44773c930ddccdfbcff8cb189
Willy Xiao <willyxiao@gmail.com>
2016-03-08 22:30:52 +0000

    simulating threading in subprocess

--- src/answer.py
+++ src/answer.py
@@ -1,23 +1,27 @@
-import threading
+import multiprocessing as mp
+import Queue
 import time
 import random
 
 # VM Objects
 class VM(object):
-    def __init__(self, name):
+    def __init__(self, name, queue):
         self.name = name
         # messages are simply logical clock times
-        self.messages = [] # python lists are thread safe, so no lock needed
-        self.VM1 = None
-        self.VM2 = None
+        self.messages = []
+
+        self.Q = queue
+        self.VM1Q = None
+        self.VM2Q = None
+
         self.time_for_instruction = None
         self.logical_clock = 1
 
-    def registerVM1(self, vm):
-        self.VM1 = vm
+    def registerVM1Queue(self, vm_queue):
+        self.VM1Q = vm_queue
 
-    def registerVM2(self, vm):
-        self.VM2 = vm
+    def registerVM2Queue(self, vm_queue):
+        self.VM2Q = vm_queue
 
     def setClockSpeed(self, cs):
         self.time_for_instruction = 1. / cs
@@ -35,7 +39,16 @@ class VM(object):
             # figure out how long the instruction took
             started = time.time()
 
-            # see if any messages in the queue
+            # simulate a thread to read messages
+            while self.time_for_instruction > (time.time() - started):
+                try:
+                    message = self.Q.get(True, self.time_for_instruction - (time.time() - started))
+                    self.messages.append(message)
+                except Queue.Empty:
+                    # if blocked for the whole clock cycle, then we must
+                    break               
+
+            # simulate a thread to process message or take an action
             if (len(self.messages) > 0):
                 # there exist messages
                 message = self.messages.pop(0)
@@ -45,48 +58,46 @@ class VM(object):
                 # roll to figure out instruction
                 roll = random.randint(1, 10)
                 if (roll == 1):
-                    self.VM1.messages.append(self.logical_clock)
-                    self.log("Sent message to " + self.VM1.name)
+                    self.VM1Q.put(self.logical_clock)
+                    self.log("Sent message to 1")
                 elif (roll == 2):
-                    self.VM2.messages.append(self.logical_clock)
-                    self.log("Sent message to " + self.VM2.name)
+                    self.VM2Q.put(self.logical_clock)
+                    self.log("Sent message to 2")
                 elif (roll == 3):
-                    self.VM1.messages.append(self.logical_clock)
-                    self.VM2.messages.append(self.logical_clock)
+                    self.VM1Q.put(self.logical_clock)
+                    self.VM2Q.put(self.logical_clock)
                     self.log("Sent message to both")
                 else:
                     self.log("Internal event")
 
                 # no messages, increment logical clock
-                self.logical_clock += 1                
-
-            # sleep for remaining amount of time
-            time_taken = time.time() - started
-            time_remaining = self.time_for_instruction - time_taken
-            if time_remaining > 0:
-                time.sleep(time_remaining)
-
+                self.logical_clock += 1
 
 # main thread of execution
 if __name__ == "__main__":
+    # create Queues for each VM to read from
+    queue1 = mp.Queue()
+    queue2 = mp.Queue()
+    queue3 = mp.Queue()
+
     # create VMs
-    v1 = VM("VM1")
-    v2 = VM("VM2")
-    v3 = VM("VM3")
+    v1 = VM("VM1", queue1)
+    v2 = VM("VM2", queue2)
+    v3 = VM("VM3", queue3)
     
     # set up the threads
-    v1.registerVM1(v2)
-    v1.registerVM2(v3)
+    v1.registerVM1Queue(queue2)
+    v1.registerVM2Queue(queue3)
     v1_clock_speed = random.randint(1,6)
     v1.setClockSpeed(v1_clock_speed)
 
-    v2.registerVM1(v1)
-    v2.registerVM2(v3)
+    v2.registerVM1Queue(queue1)
+    v2.registerVM2Queue(queue3)
     v2_clock_speed = random.randint(1,6)
     v2.setClockSpeed(v2_clock_speed)
 
-    v3.registerVM1(v1)
-    v3.registerVM2(v2)
+    v3.registerVM1Queue(queue1)
+    v3.registerVM2Queue(queue2)
     v3_clock_speed = random.randint(1,6)
     v3.setClockSpeed(v3_clock_speed)
 
@@ -96,18 +107,10 @@ if __name__ == "__main__":
         + " | VM3: " + str(v3_clock_speed) + "]")
     
     # on your mark, get set, go!
-    t1 = threading.Thread(target=v1.execution)
-    t2 = threading.Thread(target=v2.execution)
-    t3 = threading.Thread(target=v3.execution)
-
-    t1.setDaemon(True)
-    t2.setDaemon(True)
-    t3.setDaemon(True)
-
-    t1.start()
-    t2.start()
-    t3.start()
-
-    # wait for them all -- not joining so ctrl-C remains active
-    while threading.active_count() > 0:
-        time.sleep(0.1)
+    p1 = mp.Process(target=v1.execution)
+    p2 = mp.Process(target=v2.execution)
+    p3 = mp.Process(target=v3.execution)
+    
+    p1.start()
+    p2.start()
+    p3.start()
